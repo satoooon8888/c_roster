e36606a80d145796bea5b3e2cd28b78cb6a7e223
satoooon8888 <samidare256@gmail.com>
2020-11-04 14:43:36 +0900

    テストを追加

--- test/test_search.c
+++ test/test_search.c
@@ -8,31 +8,31 @@ int main(void) {
 	printf("目視で確認してください\n");
 	printf("[*] load test.csv\n");
 	load_roster_csv("./test/resource/test.csv", roster);
-	printf("[*] JHS: 茨城中学");
+	printf("[*] JHS: 茨城中学\n");
 	search_roster_JHS(roster, "茨城中学");
-    printf("[*] name: D森　太郎");
+    printf("[*] name: D森　太郎\n");
 	search_roster_name(roster, "D森　太郎");
-	printf("[*] number: 5");
+	printf("[*] number: 5\n");
 	search_roster_number(roster, 5);
 	
 	init_roster(roster);
 	printf("[*] load empty.csv\n");
 	load_roster_csv("./test/resource/empty.csv", roster);
-	printf("[*] JHS: 虚無中学");
+	printf("[*] JHS: 虚無中学\n");
 	search_roster_JHS(roster, "虚無中学");
-	printf("[*] name: 虚無　太郎");
+	printf("[*] name: 虚無　太郎\n");
 	search_roster_name(roster, "虚無　太郎");
-	printf("[*] number: 0");
+	printf("[*] number: 0\n");
 	search_roster_number(roster, 0);
 	
 	init_roster(roster);
 	printf("[*] load 100.csv\n");
 	load_roster_csv("./test/resource/100.csv", roster);
-	printf("[*] JHS: 第100中学");
+	printf("[*] JHS: 第100中学\n");
 	search_roster_JHS(roster, "第100中学");
-	printf("[*] name: 第100中学");
+	printf("[*] name: 第100中学\n");
 	search_roster_name(roster, "第100中学");
-	printf("[*] number: 100");
+	printf("[*] number: 100\n");
 	search_roster_number(roster, 100);
 	
 	return 0;
